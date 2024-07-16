b44a510e43c0f6bee480d134205dfae76c4bd030
myChinesePYL <3223285598@qq.com>
2024-07-16 17:30:20 +0800

    Emm

---
+++ src/core/Color.rs
@@ -0,0 +1,9 @@
+struct Color {
+  test: i32
+}
+
+impl Color {
+  pub fn new() -> Color {
+    println!("Hello class");
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[warn(non_snake_case)]
+
 fn main() {
-    println!("Hello, world!");
+    let UV = vec![1,2,3];
+    println!("Run finish: {}", &UV[2]);
 }

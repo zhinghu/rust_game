fcc01335ff38bc07d06e2f6bf25bf363f43643ab
myChinesePYL <3223285598@qq.com>
2024-07-21 16:51:11 +0800

    学会了导入库和读取一些信息

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+include!("core/Color.rs");
+include!("core/vec.rs");
+
 fn main() {
-    let num: i8 =
+    println!("Version: {}-{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+    println!("Author: Github mychinesepyl\n");
 }

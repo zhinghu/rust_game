18f22cb9409374bbe5e552b5b9b16253cf5e3719
myChinesePYL <3223285598@qq.com>
2024-07-22 16:58:30 +0800

    搞了个向量转颜色的功能(真难)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-include!("core/Color.rs");
 include!("core/vec.rs");
 
 fn main() {
     println!("Version: {}-{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
     println!("Author: Github mychinesepyl\n");
+
+    let test_vec_a: Vec<f32> = vec![-0.9, 0.0, 0.9, -1.0];
+    let vec_rgb = vec::to_rgb(test_vec_a);
+
+    println!("Color: {}, {}, {}, {}", vec_rgb[0], vec_rgb[1], vec_rgb[2], vec_rgb[3]);
 }

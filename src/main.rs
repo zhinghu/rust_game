13976f3c0511fd03e56bcdddc48ee4bbdc6e0503
myChinesePYL <3223285598@qq.com>
2024-07-24 17:22:43 +0800

    自己写向量太难了, 还是用ndarray吧...

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-include!("core/vec.rs");
-
 fn main() {
     println!("Version: {}-{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
-    println!("Author: Github mychinesepyl\n");
+    println!("Author: Github mychinesepyl");
+    println!("Use modules: ndarray(0.15.6)");
+    println!("");
 
-    let test_vec_a: Vec<f32> = vec![-0.9, 0.0, 0.9, -1.0];
-    let vec_rgb = vec::to_rgb(test_vec_a);
+    let vec_a = ndarray::Array::from(vec![1.0, 0.0, 0.5]);
+    let vec_b = ndarray::Array::from(vec![0.6, -0.2, -0.7]);
 
-    println!("Color: {}, {}, {}, {}", vec_rgb[0], vec_rgb[1], vec_rgb[2], vec_rgb[3]);
+    println!("A dot B: {}", vec_a.dot(&vec_b));
 }

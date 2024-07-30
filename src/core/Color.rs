005ddfd6eebe3a59bde6b111f2e9c9364cf88f49
myChinesePYL <3223285598@qq.com>
2024-07-30 16:18:49 +0800

    删除ndarray库, 修改了Color

--- src/core/Color.rs
+++ src/core/Color.rs
@@ -1,25 +1,20 @@
 pub struct Color {
-    pub r: u8,
-    pub g: u8,
-    pub b: u8,
-    pub a: u8,
+    pub vec4: [f32; 4],
 }
 
 impl Color {
-    /**
-     * color_vec: -1 ~ 1(0 ~ 255)
-     */
-    pub fn new(color_vec: &[f32; 4]) -> Color {
-        let color = Color {
-            r: ((color_vec[0] + 1.0) * 127.5) as u8,
-            g: ((color_vec[1] + 1.0) * 127.5) as u8,
-            b: ((color_vec[2] + 1.0) * 127.5) as u8,
-            a: ((color_vec[3] + 1.0) * 127.5) as u8,
-        };
-        color
+    pub fn new(vec4: &[f32; 4]) -> Color {
+        Color {
+            vec4: *vec4,
+        }
     }
+    pub fn get(&self) -> [u8; 4]{
+        let vec4: [f32; 4] = self.vec4;
+        let mut result: [u8; 4] = [0, 0, 0, 0];
+        for n in 0..vec4.len() {
+            result[n] = ((vec4[n].clamp(-1.0, 1.0) + 1.0) * 127.5) as u8;
+        }
 
-    pub fn get(&self) -> [u8; 4] {
-        [self.r, self.g, self.b, self.a]
+        result
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,11 @@ include!("core/Color.rs");
 fn main() {
     println!(
         "Version: {}-{}
-Author: Github mychinesepyl
-Use modules: ndarray(0.15.6)",
+Author: Github mychinesepyl",
         env!("CARGO_PKG_NAME"),
         env!("CARGO_PKG_VERSION")
     );
     println!("");
-    let color = Color::new(&[-1.0, 1.0, 0.7, -0.3]);
-    print!("Test Color: [");
-    for elem in color.get() {
-        print!("{},", elem);
-    }
-    print!("\x1b[1D]");
-    println!("");
+    let color = Color::new(&[-1.0, 0.1, 0.7, -0.3]);
+    println!("Color: {:?}", color.get());
 }

5d627f2d25b66dfda4fb83676e9bee1606a8b6a4
myChinesePYL <3223285598@qq.com>
2024-08-02 10:16:20 +0000

    去掉警告

--- src/core/Vec.rs
+++ src/core/Vec.rs
@@ -1,7 +1,7 @@
 pub mod vec {
-    pub type vec2 = [f32; 2];
-    pub type vec3 = [f32; 3];
-    pub type vec4 = [f32; 4];
+    pub type Vec2 = [f32; 2];
+    pub type Vec3 = [f32; 3];
+    pub type Vec4 = [f32; 4];
 
     pub fn dot(a: &[f32], b: &[f32]) -> f32 {
         assert_eq!(a.len(), b.len());

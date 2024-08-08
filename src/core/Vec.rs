cd5fa8b70ef8f1f3f77ab6d566e4f3a41f88e42b
myChinesePYL <3223285598@qq.com>
2024-08-08 17:05:52 +0800

    添加了单元测试和其他的

--- src/core/Vec.rs
+++ src/core/Vec.rs
@@ -8,21 +8,44 @@ pub mod vec {
         a.iter().zip(b.iter()).map(|(&v1, &v2)| v1 * v2).sum()
     }
 
-    pub fn add(a: &[f32], b: &[f32]) -> Vec4 {
+    pub fn add(a: &[f32], b: &[f32]) -> Vec<f32> {
         assert_eq!(a.len(), b.len());
-        a.iter().zip(b.iter()).map(|(&v1, &v2)| v1 + v2).collect::<Vec4>()
+        let mut result = Vec::new();
+
+        for n in 0..a.len() {
+            result.push(a[n] + b[n]);
+        }
+
+        result
     }
 
-    pub fn sub(a: &[f32], b: &[f32]) -> Vec4 {
-        assert_eq!(a.len(), b.len());
-        a.iter().zip(b.iter()).map(|(&v1, &v2)| v1 - v2).collect::<Vec4>()
-    }
-
-    pub fn scalar_mul(a: &[f32], scalar: f32) -> Vec4 {
-        a.iter().map(|&v| v * scalar).collect::<Vec4>()
-    }
-
+    // 向量的模长(testing)
     pub fn magnitude(a: &[f32]) -> f32 {
         a.iter().map(|&v| v * v).sum::<f32>().sqrt()
+    }
+}
+
+#[cfg(test)]
+mod vec_test {
+    use super::vec;
+
+    #[test]
+    fn dot() {
+        let a = &[1.0, -1.0];
+        let b = &[1.0, 1.0];
+        assert_eq!(vec::dot(a, b), 0.0);
+    }
+
+    #[test]
+    fn add() {
+        let a = &[1.0, -1.0];
+        let b = &[1.0, 1.0];
+        assert_eq!(vec::add(a, b), vec![2.0, 0.0]);
+    }
+
+    #[test]
+    fn magnitude() {
+        let a = &[1.0, 0.0];
+        assert_eq!(vec::magnitude(a), 1.0);
     }
 }

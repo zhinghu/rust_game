49f00f5ffdf1dd1dad3d8e4b8c21df0c89d21880
myChinesePYL <3223285598@qq.com>
2024-08-13 11:45:47 +0800

    ...

--- src/core/Color.rs
+++ src/core/Color.rs
@@ -4,12 +4,10 @@ pub struct Color {
 
 impl Color {
     pub fn new(vec4: &[f32; 4]) -> Color {
-        Color {
-            vec4: *vec4,
-        }
+        Color { vec4: *vec4 }
     }
     // Get RGBA values(0-255)
-    pub fn get(&self) -> [u8; 4]{
+    pub fn get(&self) -> [u8; 4] {
         let vec4: [f32; 4] = self.vec4;
         let mut result: [u8; 4] = [0, 0, 0, 0];
         for n in 0..vec4.len() {

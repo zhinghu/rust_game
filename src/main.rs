c00f294f9cc7fd27aee8380c73fe3729b1ae0f3c
myChinesePYL <3223285598@qq.com>
2024-08-15 08:47:39 +0800

    我Color.rs文件呢¿

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,4 @@ Author: Github mychinesepyl",
         env!("CARGO_PKG_VERSION")
     );
     println!("");
-    let v1 = ndarray::arr1(&[1.0, 0.7]);
-    let v2 = ndarray::arr1(&[0.0, -0.7]);
-
-    println!("{:?}", v1.dot(&v2))
 }

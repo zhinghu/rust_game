4f666d26f047c7dd5298dc57fef018431110f239
myChinesePYL <3223285598@qq.com>
2024-08-16 11:47:11 +0800

    完成了向量转颜色

--- src/kernel/color.rs
+++ src/kernel/color.rs
@@ -1,5 +1,14 @@
 use ndarray::*;
 
-pub fn to_rgba(vec4: ArrayBase<OwnedRepr<&[f64; 4]>, Dim<[usize; 1]>>) {
-    //
+pub fn to_rgba(
+    mut vec4: ArrayBase<OwnedRepr<f64>, Dim<[usize; 1]>>,
+) -> ArrayBase<OwnedRepr<f64>, Dim<[usize; 1]>> {
+    // 防止传入一个长度为4的向量
+    assert_eq!(vec4.len(), 4);
+    // 将范围设置到-1.0 ~ 1.0
+    vec4 = vec4.map(|&x| x.clamp(-1.0, 1.0));
+
+    let ones = arr1(&[1., 1., 1., 1.]);
+    let rgba = arr1(&[127.5, 127.5, 127.5, 127.5]);
+    &rgba * (&vec4 + &ones)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use ndarray::arr1;
+
 mod kernel;
 
 fn main() {
@@ -8,4 +10,5 @@ Author: Github mychinesepyl",
         env!("CARGO_PKG_VERSION")
     );
     println!("");
+    println!("{:?}", kernel::color::to_rgba(arr1(&[-1.0, 2.0, 0.7, 0.0])));
 }

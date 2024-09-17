562bc20f6efb4005bf2184ae1e1d6074290a188b
myChinesePYL <3223285598@qq.com>
2024-09-17 09:30:13 +0800

    添加了ctrlc库，隐藏光标

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,14 @@ mod kernel;
 mod shader;
 
 fn main() {
+    ctrlc::set_handler(|| {
+        print!("\x1b[?47l\x1b[?25h");
+        std::process::exit(0);
+    })
+    .unwrap();
+
     println!(
-        "Version: {}-{}
+        "\x1b[?47h\x1b[?25l\x1b[0KVersion: {}-{}
 Author: Github mychinesepyl",
         env!("CARGO_PKG_NAME"),
         env!("CARGO_PKG_VERSION")

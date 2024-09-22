c365db8167d4eb2e14b90fc36d64a171a983d6ae
myChinesePYL <3223285598@qq.com>
2024-09-22 16:42:14 +0800

    添加帧缓冲结构体

--- src/kernel/render.rs
+++ src/kernel/render.rs
@@ -1,16 +1,30 @@
 use std::sync::{Mutex, RwLock};
 
 lazy_static::lazy_static! {
-    static ref pixels: RwLock<Vec<glm::Vector3<f32>>> = {RwLock::new(Vec::with_capacity(
+    static ref pixels: RwLock<Vec<FrameBuffer>> = {RwLock::new(Vec::with_capacity(
         (termsize::get().unwrap().cols * termsize::get().unwrap().rows) as usize,
     ))};
 }
 static INITED: Mutex<bool> = Mutex::new(false);
 
+#[derive(Clone)]
+pub struct FrameBuffer {
+    pub color: glm::Vector3<f32>,
+    pub deepin: f32,
+}
+impl FrameBuffer {
+    pub fn new(color: glm::Vector3<f32>, deepin: f32) -> FrameBuffer {
+        FrameBuffer { color, deepin }
+    }
+}
+
 pub fn init() {
     *INITED.lock().unwrap() = true;
     pixels.write().unwrap().resize(
         (termsize::get().unwrap().cols * termsize::get().unwrap().rows) as usize,
-        glm::vec3(-1.0, -1.0, -1.0),
+        FrameBuffer::new(
+            glm::vec3(-1.0, -1.0, -1.0),
+            0.0,
+        ),
     );
 }
---
+++ src/kernel/whatC.rs
@@ -0,0 +1,8 @@
+type int=i16;
+type float=f32;
+type char=i8;
+type short=i16;
+type double=f64;
+type long=i32;
+type long_long=i64;
+type boolean=bool;
